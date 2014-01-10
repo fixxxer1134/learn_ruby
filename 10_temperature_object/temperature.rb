f294f15db3bb3d9604eeb4f87b5b873417fdb9b3
fixxxer1134 <fixxxer7734@gmail.com>
2014-01-10 16:23:24 -0600

    moving on to the next one

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -1,29 +1,17 @@
 
 class Temperature
 
-	attr_accessor :f, :c
-
 	def initialize(temp = {})
 		@temp = temp
 	end
 
   	def in_fahrenheit
-  		if @temp.key?(:c)
-			@c = @temp[:c]
-			else
-			@f = ctof(@c)
-		end
-		@f
-  	end
+        @temp.key?(:f) ? @temp[:f] : ctof(@temp[:c]) 
+    end
 
-  	def in_celsius
-  		if @temp.key?(:f)
-			@f = @temp[:f]
-			else
-			@c = ftoc(@f)
-		end
-  		@c 	
-  	end
+    def in_celsius
+        @temp.key?(:c) ? @temp[:c] : ftoc(@temp[:f])
+    end
 
 	def self.from_celsius(t)
 		self.new(:c => t)
@@ -33,12 +21,12 @@ class Temperature
 		self.new(:f => t)
 	end
 
-	def ftoc temp
-		((temp - 32) * 5.0/9.0)
+	def ftoc t
+		((t - 32) * 5.0/9.0)
 	end
 
-	def ctof temp
-		(temp * 9.0/5.0) + 32
+	def ctof t
+		(t * 9.0/5.0) + 32
 	end
 end
 
@@ -54,54 +42,8 @@ end
 
 class Fahrenheit < Temperature
 
-
-end
-
-=begin
-class Temperature
-    def initialize(options = {})
-        @options = options
-    end
-
-    def in_fahrenheit
-        @options.key?(:f) ? @options[:f] : ctof(@options[:c]) #(@options[:c] * 9.0 / 5) + 32
-    end
-
-    def in_celsius
-        @options.key?(:c) ? @options[:c] : ftoc(@options[:f]) #(@options[:f] - 32) * 5.0 / 9
-    end
-
-    def self.from_fahrenheit(num)
-        self.new(:f => num)
-    end
-
-    def self.from_celsius(num)
-        self.new(:c => num)
-    end
-
-    def ftoc temp
-		((temp - 32) * 5.0/9.0)
-	end
-
-	def ctof temp
-		(temp * 9.0/5.0) + 32
+	def initialize(t, temp = {})
+		@temp = temp
+		@temp[:f] = t
 	end
 end
-
-
-class Celsius < Temperature
-
-    def initialize(num, options = {})
-        @options = options
-        @options[:c] = num
-    end
-end
-
-class Fahrenheit < Temperature
-
-    def initialize(num, options = {})
-        @options = options
-        @options[:f] = num
-    end
-end
-=end
